b2dc9751a54ce95e2997f0c610e1d30c58fcc723
Daniel Stöcklein <3005650@stud.hs-mannheim.de>
2025-08-16 16:43:41 +0200

    feat: enhance graph component with optional d3 decay settings and code style improvements

--- apps/frontend/src/components/Graph.tsx
+++ apps/frontend/src/components/Graph.tsx
@@ -1,27 +1,29 @@
-"use client";
-import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
-import {GraphData} from "force-graph";
+'use client'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/navigation'
+import { GraphData } from 'force-graph'
 
-const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), {
-  ssr: false,
-});
+const ForceGraph2D = dynamic(() => import('react-force-graph-2d'), {
+    ssr: false,
+})
 
 export default function Graph({ data }: { data: GraphData | null }) {
-  const router = useRouter();
-  if(data)
-  return (
-    <ForceGraph2D
-      graphData={data}
-      height={400}
-      width={800}
-      nodeColor={(_) => "rgb(235, 110, 60)"}
-      onNodeClick={(node) => {
-        const id = node.id;
-        if (!id) throw new Error("Node has to have a id");
+    const router = useRouter()
+    if (data)
+        return (
+            <ForceGraph2D
+                graphData={data}
+                height={400}
+                width={800}
+                nodeColor={(_) => 'rgb(235, 110, 60)'}
+                onNodeClick={(node) => {
+                    const id = node.id
+                    if (!id) throw new Error('Node has to have a id')
 
-        router.push(`/documents/${String(id)}`);
-      }}
-    />
-  );
+                    router.push(`/documents/${String(id)}`)
+                }}
+                d3VelocityDecay={0.8} // (optional) slows down drift
+                d3AlphaDecay={0.05} // (optional) helps stabilization
+            />
+        )
 }
